f8f89537d673440238708ac6030a267b92023313
Dong Seok Lee <dongseok.lee.log@gmail.com>
2022-12-09 00:25:46 +0900

    feat: 9.2 임베디드 타입

--- relationalMapping/src/main/java/domain/Member.java
+++ relationalMapping/src/main/java/domain/Member.java
@@ -21,6 +21,30 @@ public class Member extends BaseEntity{
     @Column(name = "USERNAME")
     private String username;
 
+    //기간(Period)
+//    private LocalDateTime startDate;
+//    private LocalDateTime endDate;
+    @Embedded
+    private Period workPeriod;
+
+    //주소(Address)
+//    private String city;
+//    private String street;
+//    private String zipcode;
+    @Embedded
+    private Address homeAddress;
+
+    @Embedded// 한 엔티티에서 같은 값 타입을 사용하면 중복된 컬럼 매핑으로 에러가 나서 아래와 같이 설정한다.
+    @AttributeOverrides({
+            @AttributeOverride(name = "city",
+                    column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street",
+                    column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode",
+                    column = @Column(name = "WORK_ZIPCODE"))
+    })
+    private Address workAddress;
+
 //    @Column(name = "TEAM_ID")
 //    private Long teamId;
 
