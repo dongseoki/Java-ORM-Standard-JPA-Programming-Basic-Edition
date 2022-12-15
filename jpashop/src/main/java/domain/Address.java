aaa8aeb171cd792989a89c405a5933e67056d2c5
dongseoki <dongseok.lee.log@gmail.com>
2022-12-15 21:44:46 +0900

    FEATURE: 실전 예제6. 값 타입 매핑

---
+++ jpashop/src/main/java/domain/Address.java
@@ -0,0 +1,18 @@
+package domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipCode;
+}
--- jpashop/src/main/java/domain/Delivery.java
+++ jpashop/src/main/java/domain/Delivery.java
@@ -10,9 +10,14 @@ public class Delivery  extends BaseEntity{
     @Id @GeneratedValue
     private Long id;
 
-    private String city;
-    private String street;
-    private String zipcode;
+//    private String city;
+//    private String street;
+//    private String zipcode;
+
+    // 위의 3가지 주소정보를 Address라는 값타입으로 대체
+    @Embedded // 생략 가능
+    private Address address;
+
     private DeliveryStatus status;
 
     @OneToOne(mappedBy = "delivery", fetch = LAZY)
